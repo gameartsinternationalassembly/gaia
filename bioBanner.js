f975d804f9c3d7f5838798bd3b5b2cd2d859247b
Marie Claire LeBlanc Flanagan <marieflanagan@gmail.com>
2024-07-17 15:40:39 -0400

    2024

--- bioBanner.js
+++ bioBanner.js
@@ -1,42 +1,47 @@
-  let currentBio = 0;
-  showBio(currentBio);
+let currentBio = getRandomBioIndex();
+showBio(currentBio);
 
-  function showBio(index) {
-    const bios = document.getElementsByClassName("bioCard");
-    if (index >= bios.length) {
-      currentBio = 0;
-    } 
-    if (index < 0) {
-      currentBio = bios.length - 1;
-    }
-    for (let i = 0; i < bios.length; i++) {
-      bios[i].style.display = "none";
-    }
-    bios[currentBio].style.display = "flex";
+function showBio(index) {
+  const bios = document.getElementsByClassName("bioCard");
+  if (index >= bios.length) {
+    currentBio = 0;
+  } 
+  if (index < 0) {
+    currentBio = bios.length - 1;
   }
-
-  function changeBio(n) {
-    showBio(currentBio += n);
+  for (let i = 0; i < bios.length; i++) {
+    bios[i].style.display = "none";
   }
+  bios[currentBio].style.display = "flex";
+}
 
-  function showBioById(bioId) {
-    const bios = document.getElementsByClassName("bioCard");
-    for (let i = 0; i < bios.length; i++) {
-      if (bios[i].id === bioId) {
-        currentBio = i;
-        showBio(i);
-        break;
-      }
+function changeBio(n) {
+  showBio(currentBio += n);
+}
+
+function showBioById(bioId) {
+  const bios = document.getElementsByClassName("bioCard");
+  for (let i = 0; i < bios.length; i++) {
+    if (bios[i].id === bioId) {
+      currentBio = i;
+      showBio(i);
+      break;
     }
   }
+}
 
-  document.addEventListener("DOMContentLoaded", function() {
-    const links = document.querySelectorAll('a[data-bio-id]');
-    links.forEach(link => {
-      link.addEventListener('click', function(event) {
-        event.preventDefault();
-        const bioId = this.getAttribute('data-bio-id');
-        showBioById(bioId);
-      });
+function getRandomBioIndex() {
+  const bios = document.getElementsByClassName("bioCard");
+  return Math.floor(Math.random() * bios.length);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  const links = document.querySelectorAll('a[data-bio-id]');
+  links.forEach(link => {
+    link.addEventListener('click', function(event) {
+      event.preventDefault();
+      const bioId = this.getAttribute('data-bio-id');
+      showBioById(bioId);
     });
   });
+});
